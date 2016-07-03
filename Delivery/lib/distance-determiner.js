c0c74d427a0eb74d4357a6485708de5ecefbbce9
LarsenW <wolflarsen10@gmail.com>
2016-07-04 01:24:41 +0300

    add order estimated time to response

---
+++ Delivery/lib/distance-determiner.js
@@ -0,0 +1,34 @@
+/**
+ * Created by dmytro on 03.07.16.
+ */
+var https = require('https');
+var config = require('../config');
+var EventEmitter = require('events').EventEmitter;
+
+var emitter = new EventEmitter();
+
+module.exports = function (from, to) {
+    determineDistance(from, to);
+    return emitter;
+}
+
+function determineDistance(from, to) {
+
+    var googleRequestOptions = {
+        host: config.get('googleApi:host'),
+        path: config.get('googleApi:path') + 'origins=' + from.lat + ',' + from.lng +
+        '&destinations=' + to.lat + ',' + to.lng + '&key=' + config.get('googleApi:key')
+    }
+
+    https.request(googleRequestOptions, callback).end();
+}
+var callback = function (response) {
+    var str = '';
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
+    response.on('end', function () {
+        emitter.emit('resultIsReady', str);
+    });
+}
+
--- Delivery/routes/index.js
+++ Delivery/routes/index.js
@@ -2,8 +2,11 @@
  * Created by dmytro on 29.06.16.
  */
 var Order = require('../models/order');
+var googleConnector = require('../lib/distance-determiner');
 
-console.log()
+var orderNotFound = {"status": "false"};
+var orderAccepted = {"status": "true"};
+
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index');
@@ -13,7 +16,7 @@ module.exports = function (app) {
         console.log(req.params.id);
         Order.findById(req.params.id, function (err, doc) {
             if (err) {
-                res.end('There is no such order');
+                res.json(orderNotFound);
             } else {
                 res.json(doc);
             }
@@ -21,14 +24,24 @@ module.exports = function (app) {
     })
 
     app.post('/order', function (req, res) {
-        //console.log(req.body);
+        console.log(req.body);
         new Order(req.body).save(function (err, doc) {
             if (err) {
                 res.sendStatus(400);
             } else {
-                res.sendStatus(200);
+                var distanceEmitter = googleConnector({lat: doc.from.lat, lng: doc.from.lng}, {
+                    lat: doc.to.lat,
+                    lng: doc.to.lng
+                });
+                distanceEmitter.on('resultIsReady', function (resultJSON) {
+                    var estimatedTime = JSON.parse(resultJSON).rows[0].elements[0].duration.text;
+                    orderAccepted.estimatedTime = estimatedTime;
+                    console.log(orderAccepted);
+                    res.json(orderAccepted);
+                });
             }
         });
     });
 
 }
+
