a1fc360f0b9b38dc667d8287665d05e40f5e858f
Kevin <yurakuzemko@gmail.com>
2016-07-04 23:38:22 +0300

    transfer +

--- Bank/public/javascripts/core.js
+++ Bank/public/javascripts/core.js
@@ -72,8 +72,15 @@ bank.controller('loginController', function ($scope, $http) {
         document.getElementById("modal").style.display = "block";
     };
 
+    $scope.transferModal = function () {
+        document.getElementById("modalTransfer").style.display = "block";
+    };
+
     $scope.closeModal = function () {
         document.getElementById("modal").style.display = "none";
+    };
+    $scope.closeModalTransfer = function () {
+        document.getElementById("modalTransfer").style.display = "none";
     };
 
     $scope.pay = function () {
@@ -91,14 +98,48 @@ bank.controller('loginController', function ($scope, $http) {
             })
     };
 
+    $scope.transfer = function () {
+        $http.post("/transfer", {
+            "to": $scope.formData.receiver,
+            "amount": $scope.formData.amount
+        })
+            .success(function (data) {
+                console.log(data);
+            })
+            .error(function (err) {
+                console.log(err);
+            })
+    };
+
     //testing
 
+    $scope.transaction = function () {
+        $http.post("/transaction", {
+            "API_KEY": "d6911f567ef734f18ea176481638cc8a",
+            "from": "5773d50ef6e5bd063d119d27",
+            "to": "5773c1b809aaa4fd2f7c4f9e",
+            "amount": 2
+        })
+            .success(function (data) {
+                console.log(data);
+            })
+            .error(function (err) {
+                console.log(err);
+            })
+    };
+
     // $scope.transaction = function () {
-    //     $http.post("/transaction", {
-    //         "API_KEY": "d6911f567ef734f18ea176481638cc8a",
-    //         "from": "5773d50ef6e5bd063d119d27",
-    //         "to": "5773c1b809aaa4fd2f7c4f9e",
-    //         "amount": 2
+    //
+    //     $http({
+    //         method: "GET",
+    //         url: "/get_transaction",
+    //         params:{
+    //             "id": "57740e570f1a20345ea4129a",
+    //             "from": "5773d50ef6e5bd063d119d27",
+    //             "to": "5773c1b809aaa4fd2f7c4f9e",
+    //             "date": "2016-06-29",
+    //             "amount": 5
+    //         }
     //     })
     //         .success(function (data) {
     //             console.log(data);
@@ -107,26 +148,5 @@ bank.controller('loginController', function ($scope, $http) {
     //             console.log(err);
     //         })
     // }
-
-    $scope.transaction = function () {
-
-        $http({
-            method: "GET",
-            url: "/get_transaction",
-            params:{
-                "id": "57740e570f1a20345ea4129a",
-                "from": "5773d50ef6e5bd063d119d27",
-                "to": "5773c1b809aaa4fd2f7c4f9e",
-                "date": "2016-06-29",
-                "amount": 5
-            }
-        })
-            .success(function (data) {
-                console.log(data);
-            })
-            .error(function (err) {
-                console.log(err);
-            })
-    }
 
 });
