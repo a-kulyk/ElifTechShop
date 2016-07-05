d3bd30ed29ffeb571260b74da1ad4f49333dd06f
LarsenW <wolflarsen10@gmail.com>
2016-07-05 14:44:32 +0300

    refactoring

---
+++ Delivery/public/js/app.js
@@ -0,0 +1,44 @@
+/**
+ * Created by dmytro on 02.07.16.
+ */
+var app = angular.module('delivery', []);
+app.controller('deliveryCtrl', function ($scope, $http) {
+    $scope.submit = function () {
+        var requestJSON = {};
+        requestJSON.title = $scope.title;
+        requestJSON.price = $scope.price;
+        requestJSON.from = {};
+        requestJSON.to = {};
+        requestJSON.from.username = $scope.fromUsername;
+        requestJSON.to.username = $scope.toUsername;
+        if (autocompleteFrom.getPlace()) {
+            /*              console.log(autocompleteFrom.getPlace());*/
+            var fromPlace = autocompleteFrom.getPlace();
+            requestJSON.from.lat = fromPlace.geometry.location.lat();
+            requestJSON.from.lng = fromPlace.geometry.location.lng();
+        }
+        if (autocompleteTo.getPlace()) {
+            console.log(autocompleteTo.getPlace());
+            var toPlace = autocompleteTo.getPlace();
+            requestJSON.to.lat = toPlace.geometry.location.lat();
+            requestJSON.to.lng = toPlace.geometry.location.lng();
+        }
+        $http.post("/order", requestJSON).success(function (data, status, headers, config) {
+            console.log(data);
+            $scope.estimatedTime = data.estimatedTime;
+        }).error(function (data, status, header, config) {
+
+        });
+    }
+});
+
+var options = {types: ["address"]};
+
+var fromAddress = document.getElementById('fromAddress');
+var autocompleteFrom = new google.maps.places.Autocomplete(fromAddress, options);
+
+var toAddress = document.getElementById('toAddress');
+var autocompleteTo = new google.maps.places.Autocomplete(toAddress, options);
+
+autocompleteFrom.setComponentRestrictions({country: 'ua'});
+autocompleteTo.setComponentRestrictions({country: 'ua'});
