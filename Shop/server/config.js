2edb060d5361cf4f59a8661f0139a977fbb10461
dev1ilmini <devilmini.ru@gmail.com>
2016-08-02 17:39:10 +0300

    fix bugs

--- Shop/server/config.js
+++ Shop/server/config.js
@@ -8,7 +8,7 @@ var config = {
     saveUninitialized: false
 },
   dbUrl: 'mongodb://andriikulyk:sicario@ds019654.mlab.com:19654/auth_db'
-  // dbUrl: 'mongodb://localhost/shop_app_new'
+  //dbUrl: 'mongodb://localhost/shop_app_new'
 };
 
 module.exports = config;
