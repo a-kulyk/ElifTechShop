c0ab0c5d461dee18ae1daf9dc6af5649594a66d4
LarsenW <wolflarsen10@gmail.com>
2016-08-17 17:24:45 +0300

    change spinner settings

--- Delivery/frontend/js/controllers/order-info-controller.js
+++ Delivery/frontend/js/controllers/order-info-controller.js
@@ -5,28 +5,25 @@ var moment = require('moment');
 
 module.exports = function (app) {
     app.controller('timeOutputCtrl', function ($rootScope, $scope, $routeParams, $http, orderStates) {
-        var target = document.getElementById('map');
-        var spinner = new Spinner({color: '#333333', lines: 12, position: 'absolute'});
-        spinner.spin(target);
-        $rootScope.makeOrderActiveClass = '';
-        $rootScope.trackOrderActiveClass = '';
-        $rootScope.historyActiveClass = '';
         if ($routeParams.trackingCode) {
             $scope.trackingCode = $routeParams.trackingCode;
             $http.get('/order/' + $routeParams.trackingCode).success(function (data, status, headers) {
                 if (data.success) {
-                    spinner.stop();
+                    $scope.isInfoVisible = true;
                     $scope.arrivalTime = moment(data.arrivalTime).format('DD.MM.YY, HH:mm');
                     var tempTime = moment.duration(data.travelTime, 'seconds');
                     console.log(data.travelTime);
-                    $scope.travelTime = tempTime.hours() + ' hours ' + tempTime.minutes()+' minutes';
+                    $scope.travelTime = tempTime.hours() + ' hours ' + tempTime.minutes() + ' minutes';
                     $scope.state = orderStates.statesArray[data.state];
                     $scope.fromUsername = data.from.username;
                     $scope.toUsername = data.to.username;
                     var coordinates = {};
                     coordinates.from = data.from;
                     coordinates.to = data.to;
+
                     initMap(coordinates);
+                } else {
+
                 }
                 console.log(data);
             }).error(function (data, status, headers) {
@@ -39,11 +36,13 @@ module.exports = function (app) {
         var directionsService = new google.maps.DirectionsService;
         var map = new google.maps.Map(document.getElementById('map'), {});
         directionsDisplay.setMap(map);
-
-        calculateAndDisplayRoute(directionsService, directionsDisplay, coordinates);
+        var target = document.getElementById('map');
+        var spinner = new Spinner({color: '#333333', lines: 12, top: '190px', left: '250px', position: 'absolute'});
+        spinner.spin(target);
+        calculateAndDisplayRoute(directionsService, directionsDisplay, coordinates, spinner);
     }
 
-    function calculateAndDisplayRoute(directionsService, directionsDisplay, coordinates) {
+    function calculateAndDisplayRoute(directionsService, directionsDisplay, coordinates, spinner) {
         var selectedMode = 'DRIVING';
         directionsService.route({
             origin: {lat: coordinates.from.lat, lng: coordinates.from.lng},
@@ -51,6 +50,7 @@ module.exports = function (app) {
             travelMode: google.maps.TravelMode[selectedMode]
         }, function (response, status) {
             if (status == google.maps.DirectionsStatus.OK) {
+                spinner.stop();
                 directionsDisplay.setDirections(response);
             } else {
                 window.alert('Directions request failed due to ' + status);
