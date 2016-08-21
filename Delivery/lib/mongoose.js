b677d1b3f4efa8b6e3af9f949c2eac159c9b2274
LarsenW <wolflarsen10@gmail.com>
2016-08-21 22:33:47 +0300

    split tests db and production db

--- Delivery/lib/mongoose.js
+++ Delivery/lib/mongoose.js
@@ -2,15 +2,17 @@
 
 let mongoose = require('mongoose');
 let config = require('../config');
+let uri = process.env.NODE_ENV == 'production' ? config.get('mongoose:production_uri')
+    : config.get('mongoose:development_uri');
 
-mongoose.connect(config.get('mongoose:uri'));
+mongoose.connect(uri);
 
 let db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function () {
- console.log('connected to'+' '+config.get('mongoose:uri'));
- });
+    console.log('connected to' + ' ' + uri);
+});
 
 module.exports = mongoose;
