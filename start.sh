a1fe79358bbb178a8a13725db3164009f318199d
Kevin <kevin@debian.kevin>
2016-08-23 19:31:44 +0300

    fix permissions

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ubuntu/EliftechShop/Bank
-export NODE_ENV=production
-nonhup npm start &
-cd ../
--- stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-killall node
-cd /home/ubuntu/EliftechShop
-sudo rm -r Bank
